c21834c60c53d422616553fb463d0ccf369b7965
prabal kuinkel <prabalkuinkel10@gmail.com>
2022-11-30 08:46:57 +0545

    try

---
+++ secondcsharp/WinFormsApp1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;initial catalog=testone;user id=sa;password=kist@123;");
+
+        public object sqlda { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string query = "insert into student values('prabalsir','thali','984781293','bba')";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("saved successfully");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.InnerException);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string query = "Update student set name='swornimthapa',address='mulpani' where id='5'";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("saved successfully");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.InnerException);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string query = "delete from student where name like 'p%'";//to delete the data starting from p
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("saved successfully");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.InnerException);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+          dataGridView1.Rows.Add(textBox1.Text,textBox2.Text,textBox3.Text, textBox4.Text, textBox5.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form2 obj = new Form2();
+            obj.ShowDialog();
+
+        }
+    }
+}
---
+++ secondcsharp/WinFormsApp1/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;initial catalog=testone;user id=sa;password=kist@123;");
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "select * from student";
+            SqlCommand sqlcommand= new SqlCommand(query, con);
+            SqlDataAdapter sda = new SqlDataAdapter(sqlcommand);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
